876476ad11c8addbe8f0fc05bc27c6eb968885d1
avel <avel40m@gmail.com>
2022-04-30 10:44:37 -0300

    Detalle producto

---
+++ js/productoSimilare.js
@@ -0,0 +1,36 @@
+import { listaService } from './producto.js';
+
+const nuevaLinea = (id, imagen, nombre, precio) => {
+
+    const linea = document.createElement("div");
+    linea.classList.add("grid-item");
+    const contenido = ` <img
+    src="${imagen}"
+    alt=""
+    width="120"
+    height="120"
+  />
+  <h5>${nombre}</h5>
+  <h6>$${precio}</h6>
+  <a href="producto.html?id=${id}">Ver producto</a>`;
+  linea.innerHTML = contenido;
+  return linea
+}
+
+const productoSimilares = document.querySelector("[data-productoSimilares]");
+
+listaService.listaProducto().then((datos) => {
+    let productos = [];
+
+    for (let i = 0; i < 6; i++) {
+
+        productos.push(datos[Math.floor(Math.random() * datos.length)]);
+    }
+
+    productos.forEach(producto => {
+        const linea = nuevaLinea(producto.id,producto.imagen,producto.nombre,producto.precio);
+        productoSimilares.appendChild(linea);
+    });
+    
+}).catch(error => console.log(error));
+
