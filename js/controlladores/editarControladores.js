4f7f43f4c9713d017f14a2eeca11df398b34b652
avel <avel40m@gmail.com>
2022-05-03 11:39:29 -0300

    fix modificar

--- js/controlladores/editarControladores.js
+++ js/controlladores/editarControladores.js
@@ -1,5 +1,6 @@
 import { listaService } from '../producto.js';
 
+
 const formulario = (e) => {
     const Id = document.querySelector("[data-id]").value;
     const Nombre = document.querySelector("[data-agregarNombre]").value;
@@ -7,8 +8,10 @@ const formulario = (e) => {
     const Descripcion = document.querySelector("[data-agregarDescripcion]").value;
     const Tipo = document.querySelector("[data-agregarTipo]").value;
     const imagen = document.querySelector("[data-visulizar]").src;
-    console.log(imagen);
-    listaService.actualizarProducto(Id,Nombre,Precio,Descripcion,Tipo,imagen);
+    var arrayImagen = imagen.split("/");
+    const textoImagen = arrayImagen[3] + '/' + arrayImagen[4];
+    console.log(textoImagen);
+    listaService.actualizarProducto(Id,Nombre,Precio,Descripcion,Tipo,textoImagen);
 }
 
 export const editarDatos = {
